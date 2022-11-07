00536913f598dcd585dd4c105a71d8cba422cddd
Ricardo Luis Barbosa Pereira <patresio@gmail.com>
2022-11-07 16:51:21 -0300

    metade da aula 01

--- empresa/admin.py
+++ empresa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Tecnologia, Empresa
+from .models import Tecnologia, Empresa, Vagas
 
 # Register your models here.
 admin.site.register(Tecnologia)
 admin.site.register(Empresa)
+admin.site.register(Vagas)
