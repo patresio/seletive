63ccadc503197d562465ff4135f5a860d9eb181a
patresio <patresio@gmail.com>
2022-11-08 06:23:49 -0300

    teste

--- empresa/views.py
+++ empresa/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Tecnologia
 
 # Create your views here.
 
 def novaEmpresa(request):
-    return render(request, 'novaEmpresa.html')
+    techs = Tecnologia.objects.all()
+    return render(request, 'novaEmpresa.html', {'techs': techs})
